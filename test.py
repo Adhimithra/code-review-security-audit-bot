5bb5860812093274df7a12d8ea5c70c9c523c9fa
Adhimithra <adhimithrra@gmail.com>
2026-09-11 21:12:56 +0530

    add test code for security audit

---
+++ test.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def get_user(username):
+    query = "SELECT * FROM users WHERE name = '" + username + "'"
+    return db.execute(query).fetchall()
+
+def search_users(users, target):
+    for user in users:
+        for item in users:
+            if user == target:
+                return user
